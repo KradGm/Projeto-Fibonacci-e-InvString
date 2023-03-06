a5399cb26413efa4600804d33c72e29b4942b2e5
KradGm <enzo.loliveira@hotmail.com>
2023-03-05 23:53:16 -0300

    Commit2
    Transformando todos os códigos para HTML para mais facil visualização

--- inverte.js
+++ inverte.js
@@ -1,9 +1,10 @@
 let string = 'string';
-let inverted ='';
-//console.log(string[0]);
+let inverted ='';   //console.log(string[0]);
+const write = document.querySelector('.res');
+write.innerHTML += `O texto sendo invertido é: ${string}<br>`;
 for(var i = string.length -1;i>=0;i--){
     
     console.log(string[i])//array invertido
     inverted += string[i];
 }
-console.log(inverted);//texto invertido
+write.innerHTML += `O resultado da inversão é: ${inverted}`;//texto invertido
